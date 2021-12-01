9f30760bc3539649e6d4e68e17b24af5185894fa
Daria <dashok.myr@gmail.com>
2021-11-30 17:04:59 -0800

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './header/Header';
+import HomePage from './homePage/HomePage';
 
 function App() {
-  return <div>Header</div>;
+  return (
+    <div className="bg-main h-screen font-lora">
+      <Header />
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
+const colors = require('tailwindcss/');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      main: '#050005',
+      frame: '#1c3d46',
+      grey: '#474c52',
+      purple: '#4D194D',
+      white: '#E9ECEF',
+      fontFamily: {
+        lora: 'Lora, serif',
+      },
+    },
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      main: '#050005',
+    }),
+    textColor: (theme) => theme('colors'),
+    textColor: {
+      grey: '#474c52',
+      white: '#E9ECEF',
+      green: '#006466',
+    },
+    borderColor: (theme) => ({
+      ...theme('colors'),
+      white: '#E9ECEF',
+      green: '#006466',
+    }),
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
